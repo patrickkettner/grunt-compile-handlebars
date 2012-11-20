7d19fae5514ad0460be126157ee545f3b9bfbafb
stroutsink <mm21xx@hotmail.com>
2012-11-20 09:31:32 -0800

    Added a second pattern based task to the plugin
    This second pattern based task allows the use of a pattern for filenames
    to parse, also added grunt.template.process calls to allow the use of
    package.json values (or any other underscore code) to dynamically create
    paths if desired. More details in the comments in the file

--- tasks/compile-handlebars.js
+++ tasks/compile-handlebars.js
@@ -23,6 +23,43 @@ module.exports = function(grunt) {
 
   });
 
+  /* Batch compiles handlebars templates statically using filename patterns
+  * @author Arne G Strout, arne.strout@icrossing.com
+  * config example:
+  * 'compile-handlebars-pattern': {
+  *    development:{
+  *      files: 'app/*.hbr', // pattern to look for, output filename will be the same as the template filename with a .html extension
+  *      data: 'hbr-context/*.json', // relative to the path of the file, not the gruntfile, * replaced by filename of matched file
+  *      replaceDir: 'app/', // Replace this within the file path with the withDir value
+  *      withDir: 'dist/' // Replaces the replaceDir value 
+  *    }
+  *  }
+  */
+  grunt.registerMultiTask('compile-handlebars-pattern','Compile Handlebars templates based on pattern',function(){
+    
+    this.data.files=grunt.template.process(this.data.files);
+    this.data.withDir=grunt.template.process(this.data.withDir);
+    this.data.replaceDir=grunt.template.process(this.data.replaceDir);
+    this.data.data=grunt.template.process(this.data.data);
+    grunt.log.write("Compile Handlebars using file pattern: "+this.data.files+"\n");
+
+    var fileset=grunt.file.expandFiles(this.data.files);
+    var i=0,n,c,p,out;
+    for(i=0;i<fileset.length;i++){
+      n=fileset[i].substr(fileset[i].lastIndexOf('/')+1).split('.')[0]; // just the filename
+      p=fileset[i].substr(0,fileset[i].lastIndexOf('/')+1); // just the path
+      out=p.split(this.data.replaceDir).join(this.data.withDir)+n+'.html'; // the output file
+
+      grunt.log.write('Rendering template "'+n+'" to '+out+"...\n");
+      c=grunt.helper('compile-handlebars', {
+        template: fileset[i],
+        output: out,
+        templateData: grunt.file.readJSON(p+this.data.data.split('*').join(n))
+      });
+      grunt.file.write(out,c);
+    }
+  });
+
   // ==========================================================================
   // HELPERS
   // ==========================================================================
