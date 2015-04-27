fc164acde4a1e159550fbb264e1f86764f56bbc3
Patrick Kettner <patrickkettner@gmail.com>
2015-04-27 09:55:59 -0400

    style updates

--- tasks/compile-handlebars.js
+++ tasks/compile-handlebars.js
@@ -20,11 +20,16 @@ module.exports = function(grunt) {
 
   // Normalizes the input so that it is always an array for the forEach loop
   var getConfig = function(config) {
-    if (!config) return [];
+    if (!config) {
+      return [];
+    }
+
     var files = grunt.file.expand(config);
+
     if (files.length) {
       return files;
     }
+
     return [config];
   };
 
@@ -50,8 +55,12 @@ module.exports = function(grunt) {
 
   // Checks if the input is a glob and if so, returns the unglobbed version of the filename
   var isGlob = function(filename) {
-    if (!filename || typeof filename === 'object') return;
+    if (!filename || typeof filename === 'object') {
+      return;
+    }
+
     var match = filename.match(/[^\*]*/);
+
     if (match[0] !== filename) {
       return match.pop();
     }
@@ -66,12 +75,10 @@ module.exports = function(grunt) {
     glob = isGlob(template);
     if (outputInInput) {
       basename = _toArray(filename.split('.').pop()) ;
-    }
-    else if (glob) {
+    } else if (glob) {
       basename = filename.slice(glob.length, filename.length).split('.');
       basename.pop();
-    }
-    else {
+    } else {
       basename = filename.split('/').pop().split('.');
       basename.pop();
     }
@@ -79,14 +86,15 @@ module.exports = function(grunt) {
   };
 
   var mergeJson = function(source, globals) {
-    var json = {}, fragment;
+    var json = {}
+    var fragment;
 
-    globals.forEach(function (global) {
+    globals.forEach(function(global) {
       if (grunt.util.kindOf(global) === 'object') {
         fragment = global;
       } else {
         if (!grunt.file.exists(global)) {
-          return grunt.log.error("JSON file " + global + " not found.");
+          return grunt.log.error('JSON file ' + global + ' not found.');
         } else {
           try {
             fragment = grunt.file.readJSON(global);
@@ -96,7 +104,7 @@ module.exports = function(grunt) {
           }
         }
       }
-        _merge(json, fragment);
+      _merge(json, fragment);
     });
 
     if (typeof source === 'object') {
@@ -115,12 +123,16 @@ module.exports = function(grunt) {
     return itShould;
   };
 
-  var getTemplateData = function (templateData, filepath, index) {
+  var getTemplateData = function(templateData, filepath, index) {
     var data;
     if (Array.isArray(templateData)) {
       data = templateData[index];
-      if (data) return data;
+      if (data) {
+        return data;
+      }
+
       grunt.log.error('You need to assign the same number of data files as source files when using array notation.');
+
       return;
     }
     if (typeof templateData === 'object') {
@@ -131,24 +143,31 @@ module.exports = function(grunt) {
     }
     if (isGlob(templateData) !== undefined) {
       data = filepath.replace(path.extname(filepath), path.extname(templateData));
-      if (data) return data;
-      grunt.log.error('No matching data file for '+filepath+'.');
-      return;
+      if (data) {
+        return data;
+      }
+
+      return grunt.log.error('No matching data file for ' + filepath + '.');
     }
+
     return templateData;
   };
 
-  var getDest = function (destConfig, index) {
+  var getDest = function(destConfig, index) {
     var dest;
     if (Array.isArray(destConfig)) {
       dest = destConfig[index];
-      if (dest) return dest;
+      if (dest) {
+        return dest;
+      }
       grunt.log.error('You need to assign the same number of destination files as source files when using array notation.');
       return;
     }
     if (isGlob(destConfig) !== undefined) {
       dest = grunt.file.expand(destConfig)[index];
-      if (dest) return dest;
+      if (dest) {
+        return dest;
+      }
       grunt.log.error('You need to assign the same number of destination files as source files when using glob notation.');
       return;
     }
@@ -167,13 +186,13 @@ module.exports = function(grunt) {
     handlebarsPath = config.handlebars ? path.resolve(config.handlebars) : 'handlebars';
     handlebars = require(handlebarsPath);
 
-    helpers.forEach(function (helper) {
-      var fullPath = helper.replace(/\.[^/.]+$/, "");
+    helpers.forEach(function(helper) {
+      var fullPath = helper.replace(/\.[^/.]+$/, '');
       var name = shouldRegisterFullPaths(config.registerFullPath, 'helpers') ?
         // full path, minus extention
-          fullPath :
+        fullPath :
         // just the file's name
-          getBasename(helper, config.helpers);
+        getBasename(helper, config.helpers);
 
       if (handlebars.helpers[name] && usedHelpers.indexOf(fullPath) === -1) {
         grunt.log.error(name + ' is already registered, clobbering with the new value. Consider setting `registerFullPath` to true');
@@ -181,16 +200,16 @@ module.exports = function(grunt) {
         usedHelpers.push(fullPath);
       }
 
-        handlebars.registerHelper(name, require(fs.realpathSync(helper)));
+      handlebars.registerHelper(name, require(fs.realpathSync(helper)));
     });
 
-    partials.forEach(function (partial) {
-      var fullPath = partial.replace(/\.[^/.]+$/, "");
+    partials.forEach(function(partial) {
+      var fullPath = partial.replace(/\.[^/.]+$/, '');
       var name = shouldRegisterFullPaths(config.registerFullPath, 'partials') ?
         // full path, minus extention
-          fullPath :
+        fullPath :
         // just the file's name
-          getBasename(partial, config.partials);
+        getBasename(partial, config.partials);
 
       if (handlebars.partials[name] && usedPartials.indexOf(fullPath) === -1) {
         grunt.log.error(name + ' is already registered, clobbering with the new value. Consider setting `registerFullPath` to true');
@@ -198,16 +217,16 @@ module.exports = function(grunt) {
         usedPartials.push(fullPath);
       }
 
-      handlebars.registerPartial(name, fs.readFileSync(fs.realpathSync(partial), "utf8"));
+      handlebars.registerPartial(name, fs.readFileSync(fs.realpathSync(partial), 'utf8'));
     });
 
-    var doCompilation = function (file, filepath, index) {
-      var dest = file.dest || "";
+    var compile = function(file, filepath, index) {
+      var dest = file.dest || '';
       var template = filepath;
       var compiledTemplate = handlebars.compile(parseData(template, true));
       var templateData = getTemplateData(config.templateData, filepath, index);
       var outputPath = getDest(dest, index);
-      var appendToFile = ( outputPath === file.orig.dest && grunt.file.exists(outputPath));
+      var appendToFile = (outputPath === file.orig.dest && grunt.file.exists(outputPath));
       var operation = appendToFile ? 'appendFileSync' : 'writeFileSync';
       var html = '';
       var json;
@@ -218,13 +237,11 @@ module.exports = function(grunt) {
 
       json = mergeJson(parseData(templateData), config.globals || []);
 
-
       html += compiledTemplate(json);
 
       if (config.postHTML) {
         html += parseData(config.postHTML);
       }
-
 
       // shamefully copied straight out of grunt's file command, to allow for appending
       // http://git.io/ZYGLEw
@@ -234,31 +251,31 @@ module.exports = function(grunt) {
         fs[operation](outputPath, html);
         grunt.verbose.ok();
         return true;
-      } catch(e) {
+      } catch (e) {
         grunt.verbose.error();
         throw grunt.util.error('Unable to write "' + outputPath + '" file (Error code: ' + e.code + ').', e);
       }
 
     };
 
-    files.forEach(function (file) {
-      if(Array.isArray(file.dest) && file.dest.length > file.src.length) {
+    files.forEach(function(file) {
+      if (Array.isArray(file.dest) && file.dest.length > file.src.length) {
         if (file.src.length > 1) {
           grunt.log.error('You may only have one source file when there are more destination files than source files.');
           return;
         } else {
-          file.dest.forEach(function (destpath, index) {
-            doCompilation(file, file.src[0], index);
+          file.dest.forEach(function(destpath, index) {
+            compile(file, file.src[0], index);
           });
         }
       } else {
-        if(file.src.length > 0) {
-          file.src.forEach(function (filepath, index) {
-            doCompilation(file, filepath, index);
+        if (file.src.length > 0) {
+          file.src.forEach(function(filepath, index) {
+            compile(file, filepath, index);
           });
         } else {
-          file.orig.src.forEach(function (template, index) {
-            doCompilation(file, template, index);
+          file.orig.src.forEach(function(template, index) {
+            compile(file, template, index);
           });
         }
       }
